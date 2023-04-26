23f7f480fc122f5b065bbce57da57ed787f049e2
Shaun Inman <shaun@panic.com>
2023-04-26 19:30:06 -0400

    complete revamp of smart rendering pipeline
    with many thanks to eggs and his DE2 examples 🙏

--- workspace/trimuismart/platform/platform.c
+++ workspace/trimuismart/platform/platform.c
@@ -1,7 +1,6 @@
 // trimuismart
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
@@ -17,11 +16,127 @@
 #include "api.h"
 #include "utils.h"
 
-// TODO: flesh out based on picoarch-smart
+#include "sunxi_display2.h"
+#include "ion.h"
+#include "ion_sunxi.h"
+#include "scaler_neon.h"
+
+///////////////////////////////
+// 100% eggs
+
+#define FB_CH (0)
+#define FB_LAYER (0)
+#define FB_ZORDER (0)
+#define SCALER_CH (1)
+#define SCALER_LAYER (0)
+#define SCALER_ZORDER (10)
+#define OVERLAY_CH (2)
+#define OVERLAY_LAYER (0)
+#define OVERLAY_ZORDER (11)
+#define DEF_FB_CH (2)
+#define DEF_FB_LAYER (0)
+
+#define DE			(0x01000000)
+#define RT_MIXER0		(DE+0x00100000)
+#define OVL_V			(RT_MIXER0+0x2000+(SCALER_CH*0x1000))
+#define OVL_V_TOP_LADD0		(0x18+(SCALER_LAYER*0x30))
+
+typedef struct ion_alloc_info {
+	uint32_t		size;
+	struct ion_handle	*handle;
+	int			fd;
+	void*			padd;
+	void*			vadd;
+} ion_alloc_info_t;
+
+//
+//	allocate ion memory
+//		input: info.size
+//
+void ion_alloc(int ion_fd, ion_alloc_info_t* info) {
+	struct ion_allocation_data	iad;
+	struct ion_fd_data		ifd;
+	struct ion_custom_data		icd;
+	sunxi_phys_data			spd;
+
+	iad.len = info->size;
+	iad.align = sysconf(_SC_PAGESIZE);
+	iad.heap_id_mask = ION_HEAP_TYPE_DMA_MASK;
+	iad.flags = 0;
+	if (ioctl(ion_fd, ION_IOC_ALLOC, &iad)<0) fprintf(stderr, "ION_ALLOC failed %s\n",strerror(errno));
+	icd.cmd = ION_IOC_SUNXI_PHYS_ADDR;
+	icd.arg = (uintptr_t)&spd;
+	spd.handle = iad.handle;
+	if (ioctl(ion_fd, ION_IOC_CUSTOM, &icd)<0) fprintf(stderr, "ION_GET_PHY failed %s\n",strerror(errno));
+	ifd.handle = iad.handle;
+	if (ioctl(ion_fd, ION_IOC_MAP, &ifd)<0) fprintf(stderr, "ION_MAP failed %s\n",strerror(errno));
+
+	info->handle = iad.handle;
+	info->fd = ifd.fd;
+	info->padd = (void*)spd.phys_addr;
+	info->vadd = mmap(0, info->size, PROT_READ|PROT_WRITE, MAP_SHARED, info->fd, 0);
+	fprintf(stderr, "allocated padd: 0x%x vadd: 0x%x size: 0x%x\n", (uintptr_t)info->padd, (uintptr_t)info->vadd, info->size);
+}
+
+//
+//	free ion memory
+//
+void ion_free(int ion_fd, ion_alloc_info_t* info) {
+	struct ion_handle_data	 ihd;
+
+	munmap(info->vadd, info->size);
+	ihd.handle = info->handle;
+	if (ioctl(ion_fd, ION_IOC_FREE, &ihd)<0) fprintf(stderr, "ION_FREE failed %s\n",strerror(errno));
+}
+
+void rotate_16bpp(void* __restrict src, void* __restrict dst, uint32_t sw, uint32_t sh, uint32_t sp) {
+	// uint64_t start = getMicroseconds();
+	
+	uint16_t* s = (uint16_t*)src;
+	uint16_t* d = (uint16_t*)dst;
+	int p = sp/2;
+	
+	for (int y=0; y<sh; y++) {
+		for (int x=0; x<sw; x++) {
+			*(d + x * sh + (sh - y - 1)) = *(s + (sh-1-y) * p + (sw-1-x));
+		}
+	}
+	
+	// printf("rotate: %ius\n", (int)(getMicroseconds()-start));
+}
+
+///////////////////////////////
 
 static struct VID_Context {
+	SDL_Surface* video;
+	SDL_Surface* buffer;
 	SDL_Surface* screen;
-	int fd_fb;
+	SDL_Surface* special;
+	
+	GFX_Renderer* renderer;
+	
+	int disp_fd;
+	int fb_fd;
+	int ion_fd;
+	int mem_fd;
+	uint32_t* mem_map;
+	
+	disp_layer_config fb_config;
+	disp_layer_config buffer_config;
+	disp_layer_config screen_config;
+	ion_alloc_info_t buffer_info;
+	ion_alloc_info_t screen_info;
+	
+	int rotated_pitch;
+	int rotated_offset;
+	
+	int page;
+	int width;
+	int height;
+	int pitch;
+	
+	int cleared;
+	int resized;
 } vid;
 static int _;
 
@@ -31,30 +146,125 @@ void ADC_quit();
 SDL_Surface* PLAT_initVideo(void) {
 	ADC_init();
 	
-	putenv("SDL_VIDEO_FBCON_ROTATION=CCW");
-	putenv("SDL_USE_PAN=true");
-	
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_ShowCursor(0);
+	vid.video = SDL_SetVideoMode(FIXED_HEIGHT, FIXED_WIDTH, FIXED_DEPTH, SDL_HWSURFACE);
+	memset(vid.video->pixels, 0, FIXED_SIZE);
 	
-	vid.fd_fb = open("/dev/fb0", O_RDWR);
-	vid.screen = SDL_SetVideoMode(FIXED_WIDTH,FIXED_HEIGHT,FIXED_DEPTH,SDL_SWSURFACE);
+	vid.disp_fd = open("/dev/disp", O_RDWR);
+	vid.fb_fd = open("/dev/fb0", O_RDWR);
+	vid.ion_fd = open("/dev/ion", O_RDWR);
+	vid.mem_fd = open("/dev/mem", O_RDWR);
+	vid.mem_map = mmap(0, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, vid.mem_fd, OVL_V);
+	
+	memset(&vid.fb_config, 0, sizeof(disp_layer_config));
+	memset(&vid.buffer_config, 0, sizeof(disp_layer_config));
+
+	// wait for vsync to avoid glitch
+	ioctl(vid.fb_fd, FBIO_WAITFORVSYNC, &_);
+
+	// save and disable default FB layer first
+	uint32_t args[4] = {0, (uintptr_t)&vid.fb_config, 1, 0};
+	vid.fb_config.channel = DEF_FB_CH;
+	vid.fb_config.layer_id = DEF_FB_LAYER;
+	ioctl(vid.disp_fd, DISP_LAYER_GET_CONFIG, args);
+	
+	vid.fb_config.enable = 0;
+	ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+	
+	// intermediate buffer
+	vid.page = 1;
+	vid.width = FIXED_WIDTH;
+	vid.height = FIXED_HEIGHT;
+	vid.pitch = FIXED_PITCH;
+	
+	vid.screen_info.size = PAGE_SIZE;
+	ion_alloc(vid.ion_fd, &vid.screen_info);
+	vid.screen = SDL_CreateRGBSurfaceFrom(vid.screen_info.vadd, vid.width, vid.height, FIXED_DEPTH, vid.pitch, RGBA_MASK_565);
+	
+	// setup ch1 screeb layer (RGB565, DOUBLEBUF) ch0/1 can use scaler, but cannot use alpha
+	vid.buffer_info.size = PAGE_SIZE * PAGE_COUNT;
+	ion_alloc(vid.ion_fd, &vid.buffer_info);
+
+	vid.buffer = SDL_CreateRGBSurfaceFrom(vid.buffer_info.vadd + vid.page * PAGE_SIZE, PAGE_HEIGHT, PAGE_WIDTH, FIXED_DEPTH, PAGE_HEIGHT*FIXED_BPP, RGBA_MASK_565);
+	vid.buffer_config.channel = SCALER_CH;
+	vid.buffer_config.layer_id = SCALER_LAYER;
+	vid.buffer_config.enable = 1;
+	vid.buffer_config.info.fb.format = DISP_FORMAT_RGB_565;
+	vid.buffer_config.info.fb.addr[0] = (uintptr_t)vid.buffer_info.padd;
+	vid.buffer_config.info.fb.size[0].width  = vid.height; // PAGE_HEIGHT;
+	vid.buffer_config.info.fb.size[0].height = vid.width; // PAGE_WIDTH;
+	vid.buffer_config.info.mode = LAYER_MODE_BUFFER;
+	vid.buffer_config.info.zorder = SCALER_ZORDER;
+	vid.buffer_config.info.alpha_mode = 0; //0: pixel alpha; 1: global alpha; 2: global pixel alpha
+	vid.buffer_config.info.alpha_value = 0;
+	vid.buffer_config.info.screen_win.x = 0;
+	vid.buffer_config.info.screen_win.y = 0;
+	vid.buffer_config.info.screen_win.width  = vid.height;
+	vid.buffer_config.info.screen_win.height = vid.width;
+	vid.buffer_config.info.fb.pre_multiply = 0;
+	vid.buffer_config.info.fb.crop.x = (int64_t)0 << 32;
+	vid.buffer_config.info.fb.crop.y = (int64_t)0 << 32;
+	vid.buffer_config.info.fb.crop.width  = (int64_t)vid.height << 32;
+	vid.buffer_config.info.fb.crop.height = (int64_t)vid.width  << 32;
+	
+	args[1] = (uintptr_t)&vid.buffer_config;
+	ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+	
+	// lotta waiting for vsync...
+	ioctl(vid.fb_fd, FBIO_WAITFORVSYNC, &_);
+	// return vid.buffer;
+	
+	// trimui's SDL pukes so much debug info
+	puts("--------------------------------"); fflush(stdout);
+	
 	return vid.screen;
 }
 
 void PLAT_quitVideo(void) {
+	puts("--------------------------------"); fflush(stdout);
+
 	ADC_quit();
 	
-	close(vid.fd_fb);
+	ioctl(vid.fb_fd, FBIO_WAITFORVSYNC, &_);
+	
+	memset(vid.video->pixels, 0, FIXED_SIZE);
+	
+	SDL_FreeSurface(vid.screen);
+	SDL_FreeSurface(vid.buffer);
+	
+	// disable all channels & revert FB channel
+	vid.fb_config.enable = vid.buffer_config.enable = 0;
+	uint32_t args[4] = {0, (uintptr_t)&vid.fb_config, 1, 0};
+	ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+
+	args[1] = (uintptr_t)&vid.buffer_config;
+	ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+
+	vid.fb_config.enable = 1;
+	vid.fb_config.channel = DEF_FB_CH;
+	vid.fb_config.layer_id = DEF_FB_LAYER;
+	args[1] = (uintptr_t)&vid.fb_config;
+	ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+
+	// free & unmap & close
+	ion_free(vid.ion_fd, &vid.buffer_info);
+	munmap(vid.mem_map, sysconf(_SC_PAGESIZE));
+	close(vid.mem_fd);
+	close(vid.ion_fd);
+	close(vid.fb_fd);
+	close(vid.disp_fd);
+	
 	SDL_Quit();
 }
 
-void PLAT_clearVideo(SDL_Surface* screen) {
-	SDL_FillRect(screen, NULL, 0);
+void PLAT_clearVideo(SDL_Surface* IGNORED) {
+	if (!vid.cleared) memset(vid.screen->pixels, 0, vid.pitch * vid.height);
+	memset(vid.buffer->pixels, 0, PAGE_SIZE); 
 }
 void PLAT_clearAll(void) {
-	// TODO:
-	PLAT_clearVideo(vid.screen);
+	vid.cleared = 1;
+	PLAT_clearVideo(vid.buffer); // clear backbuffer
 }
 
 void PLAT_setVsync(int vsync) {
@@ -62,7 +272,19 @@ void PLAT_setVsync(int vsync) {
 }
 
 SDL_Surface* PLAT_resizeVideo(int w, int h, int pitch) {
-	// TODO: this will be similar to RG35XX
+	
+	SDL_FreeSurface(vid.screen);
+	vid.width = w;
+	vid.height = h;
+	vid.pitch = pitch;
+	
+	vid.screen = SDL_CreateRGBSurfaceFrom(vid.screen_info.vadd, vid.width, vid.height, FIXED_DEPTH, vid.pitch, RGBA_MASK_565);
+	memset(vid.screen->pixels, 0, vid.pitch * vid.height);
+
+	vid.resized = 1;
+
+	vid.rotated_pitch = 0;
+	if (vid.renderer) vid.renderer->src_w = 0;
 	return vid.screen;
 }
 
@@ -73,15 +295,77 @@ void PLAT_setNearestNeighbor(int enabled) {
 	// buh
 }
 void PLAT_vsync(void) {
-	// ioctl(vid.fd_fb, FBIO_WAITFORVSYNC, &_); // TODO: this slows MinUI/menu's roll but hurts FC framerate
+	ioctl(vid.fb_fd, FBIO_WAITFORVSYNC, &_);
 }
 
-void PLAT_flip(SDL_Surface* screen, int sync) {
-	SDL_Flip(screen);
+void PLAT_blitRenderer(GFX_Renderer* renderer) {
+	vid.renderer = renderer;
+	if (!vid.special || vid.special->w!=renderer->src_h || vid.special->h!=renderer->src_w || vid.special->pitch!=renderer->src_h*FIXED_BPP || !vid.rotated_pitch) {
+		if (vid.special) SDL_FreeSurface(vid.special);
+		
+		vid.special = SDL_CreateRGBSurface(SDL_SWSURFACE, renderer->src_h,renderer->src_w, FIXED_DEPTH,RGBA_MASK_565);
+		vid.rotated_pitch = vid.height * FIXED_BPP;
+		vid.rotated_offset = (renderer->dst_x * vid.rotated_pitch) + (renderer->dst_y * FIXED_BPP);
+		
+		LOG_info("PLAT_blitRenderer >> src:%p dst:%p blit:%p src:%ix%i (%i) dst:%i,%i %ix%i (%i) vid: %ix%i (%i) (%i)\n",
+			vid.renderer->src,
+			vid.renderer->dst, // unused
+			vid.renderer->blit,
+			
+			vid.renderer->src_w,
+			vid.renderer->src_h,
+			vid.renderer->src_p, // unused
+			
+			vid.renderer->dst_x,
+			vid.renderer->dst_y,
+			vid.renderer->dst_w, // unused
+			vid.renderer->dst_h, // unused?
+			vid.renderer->dst_p, // unused
+			
+			vid.width,
+			vid.height,
+			vid.pitch,
+			vid.rotated_pitch
+			);
+	}
+	rotate_16bpp(renderer->src, vid.special->pixels, renderer->src_w,renderer->src_h,renderer->src_p);
+	((scale_neon_t)renderer->blit)(vid.special->pixels, vid.buffer->pixels+vid.rotated_offset, vid.special->w,vid.special->h, vid.special->pitch, vid.renderer->dst_h, vid.renderer->dst_w,vid.rotated_pitch);
+	
+	// LOG_info("blit(%p,%p, %i,%i,%i, %i,%i,%i)\n", vid.special->pixels, vid.buffer->pixels+vid.rotated_offset, vid.special->w,vid.special->h, vid.special->pitch, vid.renderer->dst_h, vid.renderer->dst_w,vid.rotated_pitch);
+}
+
+void PLAT_flip(SDL_Surface* IGNORED, int sync) {
+	if (!vid.renderer) rotate_16bpp(vid.screen->pixels, vid.buffer->pixels, vid.width, vid.height,vid.pitch);
+	
+	vid.buffer_config.info.fb.addr[0] = (uintptr_t)vid.buffer_info.padd + vid.page * PAGE_SIZE;
+	vid.mem_map[OVL_V_TOP_LADD0/4] = (uintptr_t)vid.buffer_info.padd + vid.page * PAGE_SIZE;
+	
+	if (vid.resized) {
+		vid.buffer_config.info.fb.size[0].width  = vid.height;
+		vid.buffer_config.info.fb.size[0].height = vid.width;
+		vid.buffer_config.info.fb.crop.width  = (int64_t)vid.height << 32;
+		vid.buffer_config.info.fb.crop.height = (int64_t)vid.width  << 32;
+		uint32_t args[4] = {0, (uintptr_t)&vid.buffer_config, 1, 0};
+		ioctl(vid.disp_fd, DISP_LAYER_SET_CONFIG, args);
+		vid.resized = 0;
+	}
+	
+	vid.page ^= 1;
+	vid.buffer->pixels = vid.buffer_info.vadd + vid.page * PAGE_SIZE;
+	
 	if (sync) PLAT_vsync();
+	
+	if (vid.cleared) {
+		PLAT_clearVideo(vid.buffer);
+		vid.cleared = 0;
+	}
+	
+	vid.renderer = NULL;
 }
 
 SDL_Surface* PLAT_getVideoBufferCopy(void) {
+	// TODO: this won't work with PLAT_blitSpecial/PLAT_flipSpecial because they bypass vid.screen
+	
 	// TODO: this is just copying the backbuffer!
 	// TODO: should it be copying the frontbuffer?
 	SDL_Surface* copy = SDL_CreateRGBSurface(SDL_SWSURFACE, vid.screen->w,vid.screen->h,FIXED_DEPTH,RGBA_MASK_AUTO);
