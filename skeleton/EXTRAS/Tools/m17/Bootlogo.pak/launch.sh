7c8b818e869302780c4083b8a9d6309b6d4a9ccd
Shaun Inman <shaun@panic.com>
2023-12-04 22:31:05 -0500

    m17: added Bootlogo.pak to extras

---
+++ skeleton/EXTRAS/Tools/m17/Bootlogo.pak/launch.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+cd $DIR
+
+{
+
+LOGO_PATH=$DIR/logo.bmp
+
+if [ ! -f $LOGO_PATH ]; then
+	echo "No logo.bmp available. Aborted."
+	exit 1
+fi
+
+# read new bitmap size
+HEX=`dd if=$LOGO_PATH bs=1 skip=2 count=4 status=none | xxd -g4 -p`
+# convert to decimal (need to swap le bytes)
+COUNT=$((0x${HEX:6:2}${HEX:4:2}${HEX:2:2}${HEX:0:2}))
+if [ $COUNT -gt 32768 ]; then
+	echo "logo.bmp too large ($COUNT). Aborted."
+	exit 1
+fi
+
+OFFSET=4044800 # rev A
+SIGNATURE=`dd if=/dev/block/by-name/boot bs=1 skip=$OFFSET count=2 status=none`
+
+if [ "$SIGNATURE" = "BM" ]; then
+	echo "Rev A"
+else
+	OFFSET=4045312 # rev B
+	SIGNATURE=`dd if=/dev/block/by-name/boot bs=1 skip=$OFFSET count=2 status=none`
+	if [ "$SIGNATURE" = "BM" ]; then
+		echo "Rev B"
+	else
+		echo "Rev unknown. Aborted."
+		exit 1
+	fi
+fi
+
+# current timestamp
+DT=`date +'%Y%m%d%H%M%S'`
+# read bitmap size
+HEX=`dd if=/dev/block/by-name/boot bs=1 skip=$(($OFFSET+2)) count=4 status=none | xxd -g4 -p`
+# convert to decimal (need to swap le bytes)
+COUNT=$((0x${HEX:6:2}${HEX:4:2}${HEX:2:2}${HEX:0:2}))
+# create backup
+echo "copying $COUNT bytes from $OFFSET to backup-$DT.bmp"
+dd if=/dev/block/by-name/boot of=./backup-$DT.bmp bs=1 skip=$OFFSET count=$COUNT
+
+# inject new
+echo "injecting $LOGO_PATH"
+dd conv=notrunc if=$LOGO_PATH of=/dev/block/by-name/boot bs=1 seek=$OFFSET
+
+sync
+echo "Done."
+
+} &> ./log.txt
+
+# self-destruct
+mv $DIR $DIR.disabled
