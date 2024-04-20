c4979bd8a32d6ff3c1bd575751ea4eee8a98ccb3
beelux <75177432+beelux@users.noreply.github.com>
2024-04-20 17:36:45 +0200

    docs: always newlines in screenshots README

--- github/generate_screenshots.sh
+++ github/generate_screenshots.sh
@@ -4,18 +4,9 @@ pushd "$(dirname "$0")" >> /dev/null
 
 files=$(ls *.png)
 screenshotText="# MinUI screenshots\n\n"
-addNewline=0
 
 for file in $files; do
-    screenshotText+="<img src=\"$file\" width=320 />"
-    # Adds a newline every two screenshots
-    if [ $addNewline -eq 0 ]; then
-        screenshotText+=" "
-        addNewline=1
-    else
-        screenshotText+="\n"
-        addNewline=0
-    fi
+    screenshotText+="<img src=\"$file\" width=320 />\n"
 done
 
 echo -e $screenshotText > README.md
