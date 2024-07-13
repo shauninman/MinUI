d45b9c28c5e7b0c6f5054c78d05359725a8c9887
Shaun Inman <shaun@panic.com>
2024-07-13 12:40:28 -0400

    my282: added Bootlogo.pak

---
+++ skeleton/EXTRAS/Tools/my282/Bootlogo.pak/launch.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+LOGO_NAME="bootlogo.bmp"
+
+DIR="$(dirname "$0")"
+cd "$DIR"
+
+LOGO_PATH=$DIR/$LOGO_NAME
+if [ ! -f $LOGO_PATH ]; then
+	show.elf $DIR/res/missing.png 2
+	exit 1
+fi
+
+BLOCK_NAME=mtdblock0
+cp /dev/$BLOCK_NAME boot0
+
+VERSION=$(cat /usr/miyoo/version)
+OFFSET_PATH="res/offset-$VERSION"
+
+if [ ! -f "$OFFSET_PATH" ]; then
+	show.elf $DIR/res/abort.png 2
+	exit 1
+fi
+
+# offset is found with binwalk mtdblock0
+OFFSET=$(cat "$OFFSET_PATH")
+
+show.elf $DIR/res/updating.png 120 &
+
+gzip -k "$LOGO_PATH"
+LOGO_PATH=$LOGO_PATH.gz
+LOGO_SIZE=$(wc -c < "$LOGO_PATH")
+
+echo $LOGO_PATH $LOGO_SIZE
+
+# workaround for missing conv=notrunc support
+OFFSET_PART=$((OFFSET+LOGO_SIZE))
+dd if=boot0 of=boot0-suffix bs=1 skip=$OFFSET_PART 2>/dev/null
+dd if=$LOGO_PATH of=boot0 bs=1 seek=$OFFSET 2>/dev/null
+dd if=boot0-suffix of=boot0 bs=1 seek=$OFFSET_PART 2>/dev/null
+
+md5sum boot0
+wc -c boot0
+
+mtd write "$DIR/boot0" boot
+killall -9 show.elf
+
+rm $LOGO_PATH boot0 boot0-suffix
+mv $DIR $DIR.disabled
+
+echo "DONE."
