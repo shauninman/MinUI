3b871522a5280c026e415d3980f91d62f0a66c0b
Shaun Inman <shaun@panic.com>
2024-07-21 09:22:12 -0400

    rg_xx: fixed disabling Enable SSH.pak

--- skeleton/EXTRAS/Tools/rg35xxplus/Enable SSH.pak/launch.sh
+++ skeleton/EXTRAS/Tools/rg35xxplus/Enable SSH.pak/launch.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$(dirname "$0")"
 cd "$DIR"
+
+{
 
 # must be connected to wifi
 if [ `cat /sys/class/net/wlan0/operstate` != "up" ]; then
@@ -16,11 +18,15 @@ echo "LANG=en_US.UTF-8" > /etc/default/locale
 
 # install or update ssh server
 apt -y update && apt -y install --reinstall openssh-server
+echo "d /run/sshd 0755 root root" > /etc/tmpfiles.d/sshd.conf
 
 # enable login root:root
-echo PermitRootLogin yes >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 printf "root\nroot" | passwd root
 
-mv "$0" "$0.disabled"
-killall show.elf
+killall -9 show.elf
+
+} &> ./log.txt
+
+mv "$DIR" "$DIR.disabled"
 reboot # required for root:root to be recognized and new locale to take effect
