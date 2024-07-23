17773c978ca4e663b3dff312f4affffbe7a2a0a8
Shaun Inman <shaun@panic.com>
2024-07-22 23:11:10 -0400

    rg_xx: just noting some other info in dmenu.bin

--- skeleton/SYSTEM/rg35xxplus/paks/MinUI.pak/launch.sh
+++ skeleton/SYSTEM/rg35xxplus/paks/MinUI.pak/launch.sh
@@ -23,6 +23,8 @@ echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 0 > /sys/class/power_supply/axp2202-battery/work_led
 
 export RGXX_MODEL=`strings /mnt/vendor/bin/dmenu.bin | grep ^RG`
+# export RGXX_TIMESTAMP=`strings /mnt/vendor/bin/dmenu.bin | grep ^202`
+# export RGXX_VERSION=`strings /mnt/vendor/bin/dmenu.bin | grep -P ^V[0-9]+`
 
 #######################################
 
