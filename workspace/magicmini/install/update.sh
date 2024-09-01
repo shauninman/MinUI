cb13f03f740a2fded3c12bbe57bc7d8fb1e7c465
Shaun Inman <shaun@panic.com>
2024-09-01 16:30:32 -0400

    magicmini: updating dtb with minui was a bad idea
    creates a problem when TF1 has advanced past this dtb

--- workspace/magicmini/install/update.sh
+++ workspace/magicmini/install/update.sh
@@ -2,18 +2,9 @@
 
 # becomes /.system/magicmini/bin/install.sh
 
-DTB_NAME=rk3562-magicx-linux.dtb
-OLD_DTB=/flash/$DTB_NAME
-NEW_DTB=/storage/TF2/.system/magicmini/dat/$DTB_NAME
-
-OLD_SUM=$(md5sum "$OLD_DTB" | awk '{ print $1 }')
-NEW_SUM=$(md5sum "$NEW_DTB" | awk '{ print $1 }')
-
-if [ "$OLD_SUM" != "$NEW_SUM" ]; then
-	echo "updating dtb"
-	mount -o remount,rw /flash
-	cp "$NEW_DTB" "$OLD_DTB"
-	mount -o remount,ro /flash
-	shutdown -r now
+# clean up from an previous ill-considered update
+DTB_PATH=/storage/TF2/.system/magicmini/dat/rk3562-magicx-linux.dtb
+if [ -f "$DTB_PATH" ]; then
+	rm -rf "$DTB_PATH"
 fi
 
