24277744545174db498008c95841eba876da6da5
Shaun Inman <shaun@panic.com>
2024-09-01 11:15:22 -0400

    magicmini: invert L stick for Ry
    dtb on TF1 is updated during install/update

---
+++ workspace/magicmini/install/update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# becomes /.system/magicmini/bin/install.sh
+
+DTB_NAME=rk3562-magicx-linux.dtb
+OLD_DTB=/flash/$DTB_NAME
+NEW_DTB=/storage/TF2/.system/magicmini/dat/$DTB_NAME
+
+OLD_SUM=$(md5sum "$OLD_DTB" | awk '{ print $1 }')
+NEW_SUM=$(md5sum "$NEW_DTB" | awk '{ print $1 }')
+
+if [ "$OLD_SUM" != "$NEW_SUM" ]; then
+	mount -o remount,rw /flash
+	cp "$NEW_DTB" "$OLD_DTB"
+	mount -o remount,ro /flash
+	shutdown -r now
+fi
+
