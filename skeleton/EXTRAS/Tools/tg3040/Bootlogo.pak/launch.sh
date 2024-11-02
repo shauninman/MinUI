68a428d988d1a534d288f123988eadb7ebd9bbbf
Shaun Inman <shaun@panic.com>
2024-11-01 22:49:45 -0400

    tg3040: updated Bootlogo.pak for 20241101 ofw

--- skeleton/EXTRAS/Tools/tg3040/Bootlogo.pak/launch.sh
+++ skeleton/EXTRAS/Tools/tg3040/Bootlogo.pak/launch.sh
@@ -5,43 +5,27 @@ set -ex
 DIR=$(dirname "$0")
 cd $DIR
 
-# 396x66 (or fewer pixels)
+# 396x66 (default), 1024x768 should fit
 # Save as BMP
 # Windows
-# 32-bit
+# 32-bit (might be 24 on the Brick)
 # DO NOT flip row order
 
 {
 
 LOGO_PATH=$DIR/bootlogo.bmp
-OFFSET=2147840
-SIZE=78462
-
 if [ ! -f $LOGO_PATH ]; then
 	echo "No logo.bmp available. Aborted."
 	exit 1
 fi
 
-COUNT=$(du -k ./bootlogo.bmp | cut -f1)
-if [ $COUNT -gt $SIZE ]; then
-	echo "logo.bmp too large ($COUNT). Aborted."
-	exit 1
-fi
-
-dd if=/dev/mmcblk0p1 of=/tmp/bootlogo.sig bs=1 skip=$OFFSET count=2
-SIGNATURE=`cat /tmp/bootlogo.sig`
-if [ "$SIGNATURE" = "BM" ]; then
-	echo "Found bitmap at offset $OFFSET"
-else
-	echo "Bad offset. Aborted."
-	exit 1
-fi
-
-# inject new
-echo "injecting $LOGO_PATH"
-dd conv=notrunc if=$LOGO_PATH of=/dev/mmcblk0p1 bs=1 seek=$OFFSET
-
+BOOT_PATH=/mnt/boot/
+mkdir -p $BOOT_PATH
+mount -t vfat /dev/mmcblk0p1 $BOOT_PATH
+cp $LOGO_PATH $BOOT_PATH
 sync
+umount $BOOT_PATH
+
 echo "Done."
 
 } &> ./log.txt
