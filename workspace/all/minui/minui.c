f65df00f78214be79f3f6b40e4498a1550635c2d
= <=>
2025-03-16 17:39:47 +0100

    ff

--- workspace/all/minui/minui.c
+++ workspace/all/minui/minui.c
@@ -2009,19 +2009,19 @@ int main (int argc, char *argv[]) {
 			GFX_flip(screen);
 			dirty = 0;
 		}
-	// handle HDMI change
-	static int had_hdmi = -1;
-	int has_hdmi = GetHDMI();
-	if (had_hdmi==-1) had_hdmi = has_hdmi;
-	if (has_hdmi!=had_hdmi) {
-		had_hdmi = has_hdmi;
+		// handle HDMI change
+		static int had_hdmi = -1;
+		int has_hdmi = GetHDMI();
+		if (had_hdmi==-1) had_hdmi = has_hdmi;
+		if (has_hdmi!=had_hdmi) {
+			had_hdmi = has_hdmi;
 
-		Entry* entry = top->entries->items[top->selected];
-		LOG_info("restarting after HDMI change... (%s)\n", entry->path);
-		saveLast(entry->path); // NOTE: doesn't work in Recents (by design)
-		sleep(4);
-		quit = 1;
-	}
+			Entry* entry = top->entries->items[top->selected];
+			LOG_info("restarting after HDMI change... (%s)\n", entry->path);
+			saveLast(entry->path); // NOTE: doesn't work in Recents (by design)
+			sleep(4);
+			quit = 1;
+		}
 
 
 		
